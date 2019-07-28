82431106335ce76d963445e61200be5d53056120
chao <chaoli.job@gmail.com>
2019-07-28 10:37:07 +0200

    added __init__.py for submodule match_patterns

---
+++ gz_matcher/match_patterns/__init__.py
@@ -0,0 +1,7 @@
+# import all pattern classes here
+
+from .patterns_gz import PatternsGZ
+from .patterns_ct import PatternsCT
+from .patterns_nt import PatternsNT
+
+__all__ = ['PatternsGZ', 'PatternsCT', 'PatternsNT']
