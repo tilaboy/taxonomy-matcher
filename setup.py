51cc98550c91b410ee42c393a45ca6e42c7a32e0
chao <chaoli.job@gmail.com>
2019-08-05 10:43:31 +0200

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 NAME = "taxonomy_matcher"
-VERSION = os.environ.get("TAXONOMY_MATCHER_VERSION", '0.0.7')
+VERSION = os.environ.get("TAXONOMY_MATCHER_VERSION", '0.0.8')
 
 with open('README.rst', "r") as readme_file:
     readme = readme_file.read()
